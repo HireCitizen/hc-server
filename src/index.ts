62080d1084fe7a1c2e761fc51cbaf7f5650daa2e
David Hall <david.ryan.hall@gmail.com>
2024-12-11 13:11:16 -0800

    feat: hard coded endpoint request for active user

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,22 @@ app.get('/', (req: Request, res: Response) => {
   res.send('hello world')
 });
 
+app.get('/user/:id', (req: Request, res: Response) => {
+  res.json({
+    id: 1,
+    handle: 'doc_mercy',
+    moniker: 'Riz "Doc" Mercy',
+    email: "david.ryan.hall@gmail.com",
+    phone: '18186054161',
+    rsi_url: "https://robertsspaceindustries.com/citizens/Doc_Mercy",
+    timezone: "PST",
+    account_status: 'ACTIVE',
+    profile_image: "https://robertsspaceindustries.com/media/6zps1zlnkpojqr/heap_infobox/A1637869310_10.jpg",
+    language_code: 'EN',
+    reputation: 5
+  });
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
