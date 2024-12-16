fb97c822f3d71629852496e8be1033f89033c121
David Hall <david.ryan.hall@gmail.com>
2024-12-15 22:51:51 -0800

    chore: cleanup unused job service code

--- src/service/jobService.ts
+++ src/service/jobService.ts
@@ -188,29 +188,8 @@ export async function createJob(job: FormData): Promise<Job> {
     await conn.rollback();
     console.error("Error creating job", error);
     throw error;
+
   } finally {
     await conn.end();
   }
 }
-
-// export async function createJob(job: FormData): Promise<Job> {
-//   const conn = await mysql.createConnection(mysqlConnectionProps);
-
-//   const [row] = await conn.query<ResultSetHeader>(
-//     `INSERT INTO job (owner_id, title, description, job_type_id, language_id, status, job_start, estimated_time, amount_paid, pay_type, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-//     [1, job.title, job.description, job.jobType, 1, 'PENDING', new Date(job.jobStart), job.estimatedTime, job.amountPaid, job.payType, new Date(), new Date()]
-//   );
-
-//   const jobId = row.insertId;
-//   const roleRows: ResultSetHeader[] = [];
-
-//   const roleResp = await Promise.all(job.crewRoles.map((crewRole) => {
-//     return conn.query<ResultSetHeader>(
-//         `INSERT INTO job_crew_role_join (job_id, crew_role_id, crew_role_count) VALUES (?, ?, ?)`,
-//         [jobId, crewRole.id, crewRole.count]
-//     );
-//   }));
-//   // return job
-//   await conn.end();
-//   return {} as Job;
-// }
