75dea26b943f0a663920f81a17ebd2858c046ee0
David Hall <david.ryan.hall@gmail.com>
2024-12-16 20:38:30 -0800

    feat: add a type for incoming form data

---
+++ src/types/Forms.ts
@@ -0,0 +1,16 @@
+import { Dayjs } from "dayjs"
+import { CrewRole } from "./Job"
+
+export type FormData = {
+  title: string
+  description: string
+  jobType: string
+  amountPaid: number | ""
+  payType: string
+  jobStart: number
+  estimatedTime: number
+  timezone: string
+  jobPrivacy: "PUBLIC" | "FRIENDS" | "ORG"
+  reputationGate: boolean
+  crewRoles: CrewRole[]
+}
